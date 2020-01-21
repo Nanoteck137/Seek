38e5031f74445cd19a1f1165ad2e205559255e75
Nanoteck137 <patrik.millvik@gmail.com>
2020-01-21 22:22:25 +0100

    More on the abstraction

--- Seek/Include/Platform/Vulkan/VulkanGraphicsContext.h
+++ Seek/Include/Platform/Vulkan/VulkanGraphicsContext.h
@@ -2,14 +2,33 @@
 
 #include "Seek/Renderer/GraphicsContext.h"
 
+#include <volk.h>
+
+struct GLFWwindow;
+
 namespace Seek
 {
-    class VulkanGraphicsContext : public
+    class VulkanGraphicsContext : public GraphicsContext
     {
     public:
-        VulkanGraphicsContext();
+        VulkanGraphicsContext(GLFWwindow* windowHandle);
 
         virtual void Init() override;
         virtual void SwapBuffers() override;
-    }
+
+    private:
+        void CreateInstance();
+        void CreateDebugMessenger();
+        void PickPhysicalDevice();
+        void CreateLogicalDevice();
+
+    private:
+        GLFWwindow* m_WindowHandle = nullptr;
+
+        VkInstance m_Instance = 0;
+        VkDebugUtilsMessengerEXT m_DebugMessenger = 0;
+
+        VkPhysicalDevice m_PhysicalDevice = 0;
+        VkDevice m_Device = 0;
+    };
 }
