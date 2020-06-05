c4116d69ed888de57b55c5171258fac87fbe4f0c
Nanoteck137 <patrik.millvik@gmail.com>
2020-06-05 15:53:36 +0200

    Restructuring the project

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,4 @@
-workspace "Seek"
+workspace "Sandbox"
     architecture "x86_64"
     startproject "Sandbox"
 
@@ -6,82 +6,9 @@ workspace "Seek"
 
     flags { "MultiProcessorCompile" }
 
-outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
-
-IncludeDir = {}
-IncludeDir["GLFW"] = "Seek/Vendor/GLFW/include"
-IncludeDir["GLAD"] = "Seek/Vendor/GLAD/include"
-IncludeDir["imgui"] = "Seek/Vendor/imgui"
-IncludeDir["stb"] = "Seek/Vendor/stb"
-IncludeDir["Box2D"] = "Seek/Vendor/Box2D/include"
-
--- Header Only Includes
-IncludeDir["spdlog"] = "Seek/Vendor/spdlog/include/"
-IncludeDir["glm"] = "Seek/Vendor/glm/"
-
-group "Dependencies"
-    include "Seek/Vendor"
-
+group "Engine"
+    include "Seek"
 group ""
-
-project "Seek"
-    location "Seek"
-    kind "StaticLib"
-    language "C++"
-    cppdialect "C++17"
-    staticruntime "on"
-
-    pchheader "SeekPCH.h"
-    pchsource "Seek/Source/Seek/SeekPCH.cpp"
-    
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-    files { 
-        "%{prj.name}/Include/**.h", 
-        "%{prj.name}/Source/**.cpp" 
-    }
-
-    includedirs {
-        "%{prj.name}/Include/",
-
-        "%{IncludeDir.spdlog}",
-        "%{IncludeDir.glm}",
-
-        "%{IncludeDir.GLFW}",
-        "%{IncludeDir.GLAD}",
-        "%{IncludeDir.imgui}",
-        "%{IncludeDir.stb}",
-        "%{IncludeDir.Box2D}",
-    }
-
-    links {
-        "GLFW",
-        "GLAD",
-        "imgui",
-        "stb",
-        "Box2D",
-        "opengl32.lib",
-        "xaudio2.lib"
-    }
-
-    filter "system:windows"
-        defines { "GLFW_INCLUDE_NONE" }
-
-    filter "configurations:Debug"
-        defines { "SK_DEBUG" }
-        runtime "Debug"
-        symbols "On"
-
-    filter "configurations:Release"
-        defines { "SK_RELEASE" }
-        runtime "Release"
-        optimize "On"
-
-    filter "configurations:Dist"
-        defines { "SK_DIST" }
-        runtime "Release"
-        optimize "On"
 
 project "Sandbox"
     location "Sandbox"
@@ -102,54 +29,10 @@ project "Sandbox"
         "%{prj.name}/Include/",
         "Seek/Include/",
 
-        "%{IncludeDir.spdlog}",
-        "%{IncludeDir.glm}",
-        "%{IncludeDir.imgui}",
-        "%{IncludeDir.Box2D}",
-    }
-
-    links {
-        "Seek",
-    }
-
-    filter "configurations:Debug"
-        defines { "SK_DEBUG" }
-        runtime "Debug"
-        symbols "On"
-
-    filter "configurations:Release"
-        defines { "SK_RELEASE" }
-        runtime "Release"
-        optimize "On"
-
-    filter "configurations:Dist"
-        defines { "SK_DIST" }
-        runtime "Release"
-        optimize "On"
-
-project "Builder"
-    location "Builder"
-    kind "ConsoleApp"
-    language "C++"
-    cppdialect "C++17"
-    staticruntime "on"
-
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-    files { 
-        "%{prj.name}/Include/**.h", 
-        "%{prj.name}/Source/**.cpp"
-    }
-
-    includedirs {
-        "%{prj.name}/Include/",
-        "Seek/Include/",
-
-        "%{IncludeDir.spdlog}",
-        "%{IncludeDir.glm}",
-        "%{IncludeDir.imgui}",
-        "%{IncludeDir.Box2D}",
+        "Seek/%{IncludeDir.spdlog}",
+        "Seek/%{IncludeDir.glm}",
+        "Seek/%{IncludeDir.imgui}",
+        "Seek/%{IncludeDir.Box2D}",
     }
 
     links {
